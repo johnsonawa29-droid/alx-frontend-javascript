8669225c7734078cc4a252ee6667da197d74f26b
johnson <johnsonawa29@gmail.com>
2025-09-16 12:06:55 +0100

    yes

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
