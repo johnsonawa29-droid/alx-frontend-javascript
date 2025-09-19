6be86078f5b0eda6c106134b36cd8981824d5945
johnson <johnsonawa29@gmail.com>
2025-09-19 14:02:53 +0100

    yes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,15 +12,16 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
+
 // Define the function interface
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// Implement the function
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+// Implement the function with standard declaration
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
 
 // Example test
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
